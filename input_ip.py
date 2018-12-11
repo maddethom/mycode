b8c40693280023f2d986defa9117d2c77c1a4017
maddethom <thomas.madden@verizonwireless.com>
2018-12-11 11:49:15 -0500

    Adding files for labs 7 and 8

---
+++ input_ip.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# Written By: Thomas Madden
+# Collect Ip address information from a user, and display it back to them
+
+# Collect input rom the user
+user_input = input('Please enter an IPv4 IP address:')
+
+
+## Print out the input collected from the user
+print("You told me the IPv4 address is:" + user_input)
+
+student@beachhead:~/mycode/lab_input$ python3 input_ip.py
---
+++ lab_input/input_ip.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# Written By: Thomas Madden
+
+user_input = input('Please enter an IPv4 IP address:')
+print("You told me the IPv4 address is:" + user_input)
+
+# Collect Ip address information from a user, and display it back to them
+
+# Collect input rom the user
+user_input = input('Please enter an IPv4 IP address:')
+
+
+## Print out the input collected from the user
+print("You told me the IPv4 address is:" + user_input)
